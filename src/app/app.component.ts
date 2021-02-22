9b51eb0fec81f6f5acc082a8cccadc58e103312f
Favour Vivian Woka <vivianemma97@gmail.com>
2021-02-22 21:39:33 +0100

    fetch api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ngx-pagi';
+
+p: number = 1;
+collection: any; 
+
+constructor( private http: HttpClient) {}
+
+  ngOnInit() {
+this.getAllData();
+  }
+
+  getAllData() {
+      const url = "https://api.instantwebtools.net/v1/passenger";
+    this.http.get(url).subscribe((data: any) => {
+      console.log(data);
+      this.collection =  data;
+      this.p =  0
+    })
+  };
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgxPaginationModule} from 'ngx-pagination';
+import { HttpClientModule } from "@angular/common/http";
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

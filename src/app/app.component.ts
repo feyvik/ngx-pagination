e6fc4025e909e798e7e52af0fe58abb2e6d469da
Favour Vivian Woka <vivianemma97@gmail.com>
2021-04-05 18:12:29 +0100

    Import font awesome, add color to icon inline, change icon size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { faFilm} from '@fortawesome/free-solid-svg-icons';
+// import { fasBell } from '@fortawesome/free-solid-svg-icons'
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,30 +14,34 @@ import { HttpClient } from "@angular/common/http";
 
 export class AppComponent {
   title = 'ngx-pagi';
-  page = 1;
-  passenger: any; 
-  itemsPerPage = 6;
-  totalItems : any;
+  // page = 1;
+  // passenger: any; 
+  // itemsPerPage = 6;
+  // totalItems : any;
+  // private http: HttpClient
   
+  // filmIcon = faFilm;
+  // fasbell = fasBell;
+  // faCoffee = faCoffee;
 
-  constructor( private http: HttpClient) {}
+  constructor( ) {}
 
   ngOnInit() {
-    this.getAllData();
+    // this.getAllData();
   }
 
-  getAllData() {
-    this.http.get(`https://api.instantwebtools.net/v1/passenger?page=${1}&size=${this.itemsPerPage}`).subscribe((data: any) => {
-      this.passenger =  data.data;
-      this.totalItems = data.totalPassengers;
-    })
-  };
+  // getAllData() {
+  //   this.http.get(`https://api.instantwebtools.net/v1/passenger?page=${1}&size=${this.itemsPerPage}`).subscribe((data: any) => {
+  //     this.passenger =  data.data;
+  //     this.totalItems = data.totalPassengers;
+  //   })
+  // };
 
-  gty(page:any){
-    this.http.get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=${this.itemsPerPage}`).subscribe((data: any) => {
-      this.passenger =  data.data;
-      this.totalItems = data.totalPassengers;
-    })
-  }
+  // gty(page:any){
+  //   this.http.get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=${this.itemsPerPage}`).subscribe((data: any) => {
+  //     this.passenger =  data.data;
+  //     this.totalItems = data.totalPassengers;
+  //   })
+  // }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {NgxPaginationModule} from 'ngx-pagination';
-import {HttpClientModule} from '@angular/common/http'
+// import {NgxPaginationModule} from 'ngx-pagination';
+// import {HttpClientModule} from '@angular/common/http'
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faAngular as fabAngular, faTwitter as fabTwitter, faFacebook as fabFacebook, faReact as fabReact} from '@fortawesome/free-brands-svg-icons';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPaginationModule,
-    HttpClientModule
+    // NgxPaginationModule,
+    // HttpClientModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(fasStar, farStar, fabAngular, fabTwitter, fabFacebook, fabReact);
+  }
+}
